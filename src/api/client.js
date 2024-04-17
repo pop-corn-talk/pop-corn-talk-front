fdc51c176d97f9bcdc01147545e66e05c0abe7fd
harrisbang2 <harrisbang2@gmail.com>
2024-04-17 17:21:26 +0900

    feat/token : axios 토큰 재발급 로직 적용

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { useNavigate } from "react-router";
+
 const url = process.env.REACT_APP_API_URL;
 console.log("url", url);
 export const apiClient = axios.create({
@@ -19,3 +21,31 @@ apiClient.interceptors.request.use((config) => {
   }
   return config;
 });
+
+
+
+/////
+const updateToken = (newToken) => {
+  localStorage.setItem('access_token', newToken); // Assuming JWT is stored in localStorage
+};
+
+apiClient.interceptors.response.use((response) => {
+  console.log("response.headers");
+    console.log(response.headers);
+    if (response.headers.authorization) {
+      console.log("새로운 토큰 받기")
+      console.log(response.headers.authorization)
+      let newToken = response.headers.authorization;
+      if (newToken && newToken.startsWith('Bearer')) {
+        newToken = newToken.substring(7)
+        updateToken(newToken);
+        window.location.reload()
+      }
+    }
+    console.log(response)
+    return response;
+  },
+  (error) => {
+    console.log("axios response 오류")
+  }
+);
